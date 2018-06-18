e6d911ad25e94867507fa3125a46f18ca5264ca7
ArianaIancu <ccArianaIancu@gmail.com>
2018-06-18 09:45:45 +0300

    PART 2

--- app/src/main/java/com/example/lorenai/mycamapp/ColorFinder.java
+++ app/src/main/java/com/example/lorenai/mycamapp/ColorFinder.java
@@ -1,16 +1,10 @@
 package com.example.lorenai.mycamapp;
 
-import android.app.Activity;
-import android.app.FragmentManager;
-import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.graphics.Palette;
